a578fe01ec2d0cd4c696ceb43edc36bc02d1eef2
Shivam Singh <shiv41297@gmail.com>
2024-06-02 12:21:35 -0400

    added avatar component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  className?: string;
+  name: string;
+  onClick?: () => void;
+};
+const Avatar: React.FC<Props> = ({ className, name, onClick }) => {
+  const firstCharacter = name?.split(" ")[0]?.split("")[0];
+  const lastCharacter = name?.split(" ")[1]?.split("")[0];
+  return (
+    <div
+      className={twMerge(
+        "rounded-full bg-zinc-200 text-zinc-700 flex justify-center items-center font-medium w-full h-full",
+        className
+      )}
+      style={{ lineHeight: "1" }}
+    >
+      <button
+        className="h-full w-full"
+        onClick={onClick ? onClick : () => {}}
+      >{`${firstCharacter}${lastCharacter}`}</button>
+    </div>
+  );
+};
+
+export default Avatar;
--- src/components/Header/profile.tsx
+++ src/components/Header/profile.tsx
@@ -7,6 +7,7 @@ import { MenuButton } from "@mui/base/MenuButton";
 import { BROWSER_ROUTE } from "@/utils/Endpoints";
 import { useUserQuery } from "@/redux/slice/api/userSlice";
 import useProfile from "./useProfile";
+import AvatarComponent from "../Avatar";
 type MenuDataProps = {
   menu: string;
   link: string;
@@ -36,12 +37,11 @@ const ProfileSection = () => {
             className="h-[3rem] w-[3rem]"
           />
         ) : (
-          <Avatar
-            className="bg-primary h-[3rem] w-[3rem]"
+          <AvatarComponent
+            className="h-[3rem] w-[3rem] text-2xl"
+            name={user?.data?.name as string}
             onClick={() => setShowDropdown(!showDropdown)}
-          >{`${user?.data?.name?.split(" ")[0]?.split("")[0]}${
-            user?.data?.name?.split(" ")[1]?.split("")[0]
-          }`}</Avatar>
+          />
         )}
       </MenuButton>
       <Menu className="bg-zinc-900 text-zinc-100 rounded-lg border border-zinc-700 w-[2in] py-3 z-10">
