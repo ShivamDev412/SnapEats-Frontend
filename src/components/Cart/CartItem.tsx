a3521ea946aa2d6db024d708c1722f0dead15c1e
Shivam Singh <shiv41297@gmail.com>
2024-07-05 19:46:21 -0400

    minor ui responsiveness fix

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -57,16 +57,16 @@ const CartItem: React.FC<CartItemProps> = ({ cartItem }) => {
   const { note, handleNoteChange } = useCartItem(cartItem.note);
   return (
     <div
-      className="my-4 p-4 bg-zinc-800 rounded-lg shadow-md"
+      className="my-2 sm:my-4 p-4 bg-zinc-800 rounded-lg shadow-md"
       key={cartItem.id}
     >
       <div className="flex justify-between items-center mb-2">
-        <p className="text-lg sm:text-base font-semibold text-zinc-100">{cartItem.name}</p>
-        <p className="text-lg sm:text-base font-semibold text-zinc-100">
+        <p className="text-[1rem] sm:text-base font-semibold text-zinc-100">{cartItem.name}</p>
+        <p className="text-[1rem] sm:text-base font-semibold text-zinc-100">
           ${cartItem.price.toFixed(2)}
         </p>
       </div>
-      <div className="text-sm sm:text-xs text-zinc-400 mb-2">
+      <div className="text-xs sm:text-sm text-zinc-400 mb-2">
         {cartItem.options.map((option) => (
           <p key={option.id} className="mb-1">
             {option.optionName}: {option.choiceName} (+${option.additionalPrice})
@@ -79,11 +79,11 @@ const CartItem: React.FC<CartItemProps> = ({ cartItem }) => {
         placeholder="Add a note..."
         value={note}
         onChange={(e) => handleNoteChange(e.target.value)}
-        className="w-full p-2 mb-2 bg-zinc-600 rounded-md text-zinc-100 placeholder-zinc-400 resize-none focus:outline-none focus:ring-2 focus:ring-zinc-400 text-base sm:text-sm"
+        className="w-full p-2 mb-2 bg-zinc-600 rounded-md text-zinc-100 placeholder-zinc-400 resize-none focus:outline-none focus:ring-2 focus:ring-zinc-400 text-xs sm:text-sm"
       ></textarea>
-      <div className="flex justify-between items-center mt-2 text-sm sm:text-xs">
+      <div className="flex justify-between items-center mt-1 sm:mt-2 text-sm sm:text-xs">
         <QuantitySection quantity={cartItem.quantity} />
-        <p className="font-medium text-lg sm:text-base text-zinc-100">
+        <p className="font-medium text-[1rem] sm:text-lg text-zinc-100">
           Total: ${totalPrice.toFixed(2)}
         </p>
       </div>
