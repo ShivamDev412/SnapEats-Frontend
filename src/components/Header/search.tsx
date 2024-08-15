7c48f2669d1fafb34045078626c60c30cf0d329a
Shivam Singh <shiv41297@gmail.com>
2024-08-14 21:19:53 -0400

    fixing search functionality

--- src/components/Header/search.tsx
+++ src/components/Header/search.tsx
@@ -1,20 +1,31 @@
 import { IoSearch } from "react-icons/io5";
 import useSearch from "./useSearch";
 import { useTranslation } from "react-i18next";
-// import useDebounce from "@/Hooks/useDebounce";
-import { useGetMostOrderedItemsQuery } from "@/redux/slice/api/user/homeSlice";
 import LazyLoadedImageComponent from "../LazyLoadedImageComponent";
 import { Link } from "react-router-dom";
+import { IoIosSearch } from "react-icons/io";
 
 const Search = () => {
   const { t } = useTranslation();
-  const { search, setSearch, showSearchDropdown, setSearchDropdown } =
-    useSearch();
-  // const debouncedSearchTerm = useDebounce(search);
-  const { data: mostOrderedData, isFetching } = useGetMostOrderedItemsQuery();
+  const {
+    search,
+    setSearch,
+    showSearchDropdown,
+    setSearchDropdown,
+    handleBlur,
+    mostOrderedData,
+    isFetching,
+    handleNavigation,
+    recentSearches,
+  } = useSearch();
+
   return (
     <div className="w-full md:w-5/12 lg:w-5/12 xl:w-6/12 relative">
-      <div className="flex items-center bg-zinc-800 border border-zinc-600 rounded-[25px] p-2 gap-2 w-full md:m-0">
+      <div
+        className="flex items-center bg-zinc-800 border border-zinc-600 rounded-[25px] p-2 gap-2 w-full md:m-0"
+        onBlur={handleBlur}
+        onFocus={() => setSearchDropdown(true)}
+      >
         <IoSearch />
         <input
           type="text"
@@ -22,24 +33,41 @@ const Search = () => {
           placeholder={t("searchForFoodOrRestaurant")}
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          onFocus={() => setSearchDropdown(true)}
-          // onBlur={() => setSearchDropdown(false)}
         />
       </div>
       {showSearchDropdown && (
-        <div className="absolute sm:top-20 left-0 lg:top-10 border border-zinc-600 rounded-2xl bg-zinc-950 bg-transparent z-10 w-full h-[3in]">
-          <section className="p-4">
-            <h3 className="text-zinc-200 font-semibold">Recent search</h3>
-          </section>
-          <section className="p-4">
+        <div className="absolute sm:top-20 left-0 lg:top-10 border border-zinc-600 rounded-2xl bg-zinc-950 bg-transparent z-10 w-full h-[3in] overflow-y-auto">
+          {recentSearches.length > 0 && (
+            <section className="px-4 py-2">
+              <h3 className="text-zinc-200 font-semibold">Recent search</h3>
+              <ul className="flex flex-col gap-1 mt-2">
+                {recentSearches.map((searchItem, index) => (
+                  <li key={index}>
+                    <Link
+                      to={searchItem.url}
+                      className="text-zinc-200 flex gap-1 items-center"
+                      onClick={() => setSearchDropdown(false)}
+                    >
+                      <IoIosSearch className="h-5 w-5" />
+                      {searchItem.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </section>
+          )}
+          <section className="px-4 py-2">
             <h3 className="text-zinc-200 font-semibold text-lg">
-              Most ordered{" "}
+              Most ordered
             </h3>
             {isFetching && <p>Loading...</p>}
             <ul className="flex gap-4 my-4">
               {mostOrderedData?.data?.map((item) => (
-                <li className="w-fit ">
-                  <Link to={`/${item.storeName}/${item.storeId}`} className="flex flex-col items-center gap-2">
+                <li className="w-fit" key={item.storeId}>
+                  <button
+                    onClick={() => handleNavigation(item)}
+                    className="flex flex-col items-center gap-2"
+                  >
                     <LazyLoadedImageComponent
                       image={item.image}
                       alt={item.name}
@@ -47,7 +75,7 @@ const Search = () => {
                       className="rounded-lg w-[6rem] h-[6rem]"
                     />
                     <p className="text-center text-sm">{item.name}</p>
-                  </Link>
+                  </button>
                 </li>
               ))}
             </ul>
--- src/components/Header/useSearch.ts
+++ src/components/Header/useSearch.ts
@@ -1,8 +1,78 @@
-import { useState } from "react";
+import {
+  MostOrderedDataType,
+  useGetMostOrderedItemsQuery,
+} from "@/redux/slice/api/user/homeSlice";
+import { useUserQuery } from "@/redux/slice/api/user/profileSlice";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const useSearch = () => {
+  const navigate = useNavigate();
   const [search, setSearch] = useState("");
+  const { data: user } = useUserQuery("");
+  const userId = user?.data.id;
   const [showSearchDropdown, setSearchDropdown] = useState(false);
-  return { search, setSearch, showSearchDropdown, setSearchDropdown };
+  const [recentSearches, setRecentSearches] = useState<
+    { name: string; url: string }[]
+  >([]);
+
+  useEffect(() => {
+    if (userId) {
+      const storedSearches = JSON.parse(
+        localStorage.getItem(`recentSearches_${userId}`) || "[]"
+      );
+      setRecentSearches(storedSearches);
+    }
+  }, [userId]);
+
+  const handleBlur = () => {
+    setTimeout(() => {
+      setSearchDropdown(false);
+    }, 130);
+  };
+
+  const { data: mostOrderedData, isFetching } = useGetMostOrderedItemsQuery();
+
+  const handleNavigation = (item: MostOrderedDataType) => {
+    const url = `/${encodeURIComponent(item.storeName)}/${item.storeId}`;
+
+    const newSearch = {
+      name: item.name,
+      url: url,
+    };
+
+    const updatedSearches = [
+      newSearch,
+      ...recentSearches.filter(
+        (search: { name: string }) => search.name !== item.name
+      ),
+    ];
+
+    if (updatedSearches.length > 5) {
+      updatedSearches.pop();
+    }
+
+    localStorage.setItem(
+      `recentSearches_${userId}`,
+      JSON.stringify(updatedSearches)
+    );
+    setRecentSearches(updatedSearches);
+
+    navigate(url);
+    setSearchDropdown(false);
+  };
+
+  return {
+    search,
+    setSearch,
+    showSearchDropdown,
+    setSearchDropdown,
+    handleBlur,
+    mostOrderedData,
+    isFetching,
+    handleNavigation,
+    recentSearches, // Expose recent searches
+  };
 };
+
 export default useSearch;
